864998cdaf539a9abde5490525e068ac001bb218
Jemz <102203151+64130500111@users.noreply.github.com>
2022-09-02 21:55:08 +0700

    v1

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,22 @@
 const { template } = require('@babel/core')
 
+
 function calculateBMI(weight, height) {
-  //code here
+  return weight/(height*height)
 }
-function getBMIMeaning(weight, height) {
-  //code here
+
+function getBMIMeaning(weight, height, calculateBMI) {
+  if(calculateBMI(weight,height) < 18.5){
+      return "Underweight"
+  } else if(calculateBMI(weight,height) >= 18.5 && calculateBMI(weight,height) <= 24.9){
+     return "Normal weight"
+  } else if(calculateBMI(weight,height)> 25.0){
+    return "Overweight"
+  }
 }
+
+// console.log(calculateBMI(69, 1.68))
+// console.log(getBMIMeaning(65, 1.8, calculateBMI))
+// console.log(getBMIMeaning(80, 1.7, calculateBMI))
+// console.log(getBMIMeaning(44, 1.6, calculateBMI))
 module.exports = getBMIMeaning
